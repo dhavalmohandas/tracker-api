ddcc8dc348213d38448fecd26a3a9641ca32b18a
Dhaval <dhavalmohandas@gmail.com>
2020-07-22 17:58:39 -0400

    Update Atlas URL

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  const url = process.env.DB_URL || 'mongodb+srv://dhavalmohandas:dhaval1994@cluster0.fj8jg.mongodb.net/issuetracker?retryWrites=true';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
